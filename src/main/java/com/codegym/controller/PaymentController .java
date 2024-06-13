6bc1d0392b038bac5a3791ab28da413785c8c5b6
nguyenhuuton123 <nhtonx1@gmail.com>
2024-06-13 18:38:17 +0700

    no message

--- src/main/java/com/codegym/controller/PaymentController .java
+++ src/main/java/com/codegym/controller/PaymentController .java
@@ -13,29 +13,9 @@ public class PaymentController {
 
     // Endpoint để tạo Payment Intent
     @PostMapping("/create-payment-intent")
-    public Map<String, String> createPaymentIntent(@RequestBody Map<String, Object> data) {
+    public ResponseEntity<?> createPaymentIntent(@RequestBody Map<String, Object> data) {
 
-        // Lấy số tiền từ request body
-        int amount = (int) data.get("amount");
-
-        // Thiết lập các tham số cho PaymentIntent
-        PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
-            .setAmount((long) amount)
-            .setCurrency("usd")
-            .build();
-
-        PaymentIntent intent;
-        try {
-            // Gửi yêu cầu tới Stripe để tạo PaymentIntent
-            intent = PaymentIntent.create(params);
-        } catch (Exception e) {
-            // Xử lý ngoại lệ nếu có lỗi xảy ra khi tạo PaymentIntent
-            throw new RuntimeException(e);
-        }
-
-        // Trả về client secret cho frontend
-        Map<String, String> responseData = new HashMap<>();
-        responseData.put("clientSecret", intent.getClientSecret());
-        return responseData;
+        String clientSecret = paymentService.createPaymentIntent(data);
+	return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/codegym/service/impl/PaymentService.java
@@ -0,0 +1,38 @@
+import com.stripe.Stripe;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class PaymentService {
+
+    public Map<String, String> createPaymentIntent(@RequestBody Map<String, Object> data) {
+
+        // Lấy số tiền từ request body
+        int amount = (int) data.get("amount");
+
+        // Thiết lập các tham số cho PaymentIntent
+        PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
+            .setAmount((long) amount)
+            .setCurrency("usd")
+            .build();
+
+        PaymentIntent intent;
+        try {
+            // Gửi yêu cầu tới Stripe để tạo PaymentIntent
+            intent = PaymentIntent.create(params);
+        } catch (Exception e) {
+            // Xử lý ngoại lệ nếu có lỗi xảy ra khi tạo PaymentIntent
+            throw new RuntimeException(e);
+        }
+
+        // Trả về client secret cho frontend
+        Map<String, String> responseData = new HashMap<>();
+        responseData.put("clientSecret", intent.getClientSecret());
+        return responseData;
+    }
+}
